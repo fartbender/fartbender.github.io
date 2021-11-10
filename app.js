bbe23d71e2ed8edc4966346747f7087233e33a69
Michael Jørgensen <michael@maneno.dk>
2021-11-10 12:36:01 +0100

    dwdwa

--- app.js
+++ app.js
@@ -13,4 +13,19 @@ output.innerHTML = slider.value;
 
 slider.oninput = () => {
     output.innerHTML = slider.value;
+    publishMessage(slider.value);
+}
+
+function publishMessage(value) {
+    // With the right payload, you can publish a message, add a reaction to a message,
+    // send a push notification, or send a small payload called a signal.
+    var publishPayload = {
+        channel: "manual",
+        message: {
+            value: value
+        }
+    }
+    pubnub.publish(publishPayload, function (status, response) {
+        console.log(status, response);
+    })
 }
